c654bfc76accfd9b299d45b50e75a395691d8fec
Mikha <mjmaun.dev@gmail.com>
2024-12-08 23:29:20 +0800

    home + index updated and added fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -12,28 +9,16 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useFonts({
+		'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+		'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf'),
+		'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf')
+	})
 
-  if (!loaded) {
-    return null;
+	return (
+	  <Stack>
+		<Stack.Screen name="index" options={{ title: "Home" }} />
+	  </Stack>
+	);
   }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, }}>
+		<Text style={{ fontSize: 30, fontFamily: 'outfit-bold' }}>Testing index</Text>
+	</View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   titleContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     gap: 8,
+//   },
+//   stepContainer: {
+//     gap: 8,
+//     marginBottom: 8,
+//   },
+//   reactLogo: {
+//     height: 178,
+//     width: 290,
+//     bottom: 0,
+//     left: 0,
+//     position: 'absolute',
+//   },
+// });
