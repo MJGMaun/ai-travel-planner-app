8ada176ad4132701491666726bb360243bcc9d4d
Mikha <mjmaun.dev@gmail.com>
2024-12-09 19:48:14 +0800

    Done landing screen

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,55 +1,83 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import { Colors } from '@/constants/Colors'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { Colors } from "@/constants/Colors";
+import { StatusBar } from "expo-status-bar";
+import { useRouter } from "expo-router";
 
 export default function Login() {
-  return (
-	<View>
-	  <Image source={require('./../assets/images/login.jpg')}
-		style={{ width: '100%', height: 550}}
-		/>
 
-		<View style={styles.container}>
-			<Text style={{
-				fontSize: 28,
-				fontFamily: 'outfit_bold',
-				textAlign: 'center'
-			 }}>AI Travel Planner</Text>
+	const router = useRouter(); // needed when navigation to other screens
 
-			 <Text style={{
-				fontFamily: 'outfit',
-				fontSize: 17,
-				textAlign: 'center',
-				color: Colors.GRAY,
-				marginTop: 10,
-			 }}>Discover your next adventure effortlessly. Personalized itineraries at your fingertips. Travel smarter with AI- driven Insights.</Text>
+    return (
+        <View>
+            <StatusBar
+                style="light"
+                translucent={true}
+                backgroundColor="transparent"
+            />
+            <Image
+                source={require("./../assets/images/login.jpg")}
+                style={{ width: "100%", height: 550 }}
+            />
 
-			 <View style={styles.button}>
-				<Text style={{
-					color: Colors.WHITE,
-					textAlign: 'center',
-					fontFamily: 'outfit',
-					fontSize:17
-				}}>Sign In With Google</Text>
-			 </View>
-		</View>
-	</View>
-  )
+            <View style={styles.container}>
+                <Text
+                    style={{
+                        fontSize: 28,
+                        fontFamily: "outfit-bold",
+                        textAlign: "center",
+                    }}
+                >
+                    AI Travel Planner
+                </Text>
+
+                <Text
+                    style={{
+                        fontFamily: "outfit",
+                        fontSize: 17,
+                        textAlign: "center",
+                        color: Colors.GRAY,
+                        marginTop: 10,
+                    }}
+                >
+                    Discover your next adventure effortlessly. Personalized
+                    itineraries at your fingertips. Travel smarter with AI-
+                    driven Insights.
+                </Text>
+
+                <TouchableOpacity
+					style={styles.button}
+					onPress={()=>router.push('auth/sign-in')}
+				>
+                    <Text
+                        style={{
+                            color: Colors.WHITE,
+                            textAlign: "center",
+                            fontFamily: "outfit",
+                            fontSize: 17,
+                        }}
+                    >
+                        Get started
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: Colors.WHITE,
-		marginTop: -20,
-		borderTopLeftRadius: 30,
-		borderTopRightRadius: 30,
-		height: '100%',
-		padding: 25
-	},
-	button: {
-		padding: 15,
-		backgroundColor: Colors.PRIMARY,
-		borderRadius: 99,
-		marginTop: '15%'
-	}
-})
+    container: {
+        backgroundColor: Colors.WHITE,
+        marginTop: -20,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        height: "100%",
+        padding: 25,
+    },
+    button: {
+        padding: 15,
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 99,
+        marginTop: "15%",
+    },
+});
